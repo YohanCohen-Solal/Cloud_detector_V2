59ef3a3a4d31247511a5bcf8cc6aff71321553c5
JEOS <81352535+EOS5@users.noreply.github.com>
2023-03-10 18:41:56 +0100

    I created a very ugly flask web app and this time it works

---
+++ WebApp/flask_server/functions.py
@@ -0,0 +1,70 @@
+import os
+import tensorflow as tf
+import numpy as np
+
+from keras.models import load_model
+import torch
+import torchvision.transforms as transforms
+import numpy as np
+from keras.utils import img_to_array
+from PIL import Image
+from keras.applications.regnet import preprocess_input
+from model_ResAttUnet import ResidualAttentionUNet
+
+model = ResidualAttentionUNet(inputChannel=3, outputChannel=1)
+model.load_state_dict(torch.load('my_checkpoint.pth.h5')['state_dict'])
+model.eval()
+
+classification_model = load_model('res35_seg_model1.h5')
+classes = ["Altocumulus", "Altostratus", "Cirrocumulus", "Cirrostratus",
+           "Cirrus", "Cumulonimbus", "Cumulus", "Nimbostratus",
+           "Stratocumulus", "Stratus", "clearSky"]
+
+def preprocess_image(step, images):
+    if step == "segmentation":
+        print("je suis passer par le preprocess segmentation")
+        print("j'ai ouvert l'image")
+        width, height = images.size
+        new_width = 256
+        new_height = 256
+        pil_image = images.resize((new_width, new_height), Image.ANTIALIAS)
+        print("j'ai réussi à resize l'image")
+        to_tensor = transforms.ToTensor()
+        tensor_image = to_tensor(pil_image)
+        tensor_image = tensor_image.unsqueeze(0)
+        print("je vais retourner l'image sous forme de tenseur...")
+
+        return tensor_image
+    
+    elif step == "classification":
+        print("je suis passer par le preprocess classification")
+        print("j'ai réussi à resize l'image")
+
+        x = img_to_array(images)
+        print("j'ai réussi à convertir l'image en array")
+        x = np.expand_dims(x, axis=0)
+        print("j'ai réussi à augmenter la dimention")
+        x = preprocess_input(x)
+        print("j'ai réussi à preprocess l'imput")
+        print("je vais maintenant retourner l'image preprocess pour la classification")
+
+        return x
+
+
+def predict(step, image):
+    print(step +": ")
+    if step == "segmentation":
+        print("je suis entré dans la fonction predict pour la segmentation")
+        img = preprocess_image(step, image)
+        pred_segmentation = torch.sigmoid(model(img))
+        return pred_segmentation
+    
+    elif step == "classification":
+        print("je suis entré dans la fonction predict pour la classification")
+        img = preprocess_image(step, image)
+        preds = classification_model.predict(img)
+        pred_class = np.argmax(preds)
+        pred_class_name = classes[pred_class]
+        print("je vais maintenant retourner la classe")
+
+        return pred_class_name
---
+++ WebApp/flask_server/second_server.py
@@ -0,0 +1,76 @@
+import os
+import mimetypes
+
+from flask_cors import CORS
+from flask import Flask, flash, request, redirect, url_for, send_from_directory
+from werkzeug.utils import secure_filename
+
+import torchvision
+import keras.utils as image
+from PIL import Image
+
+from functions import *
+
+app_dir = os.getcwd()
+UPLOAD_FOLDER = "C:/Users/victo/Desktop/Cours/Cloud_detector_V2/WebApp/uploaded_file"
+SEGMENTED_FOLDER = "C:/Users/victo/Desktop/Cours/Cloud_detector_V2/WebApp/segmented_file"
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['SEGMENTED_FOLDER'] = SEGMENTED_FOLDER
+CORS(app)
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
+
+            
+            img = Image.open(file_path)
+            img_segmented = predict("segmentation", img)
+            seg_path = os.path.join(app.config['SEGMENTED_FOLDER'],filename)
+            torchvision.utils.save_image(img_segmented, seg_path)
+
+
+            img = image.load_img(seg_path, target_size=(224, 224))
+            classification = predict("classification", img)
+            return classification
+            
+
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    print("Current Workdir: " + app_dir)
+    print("jsp: "+ app.config['UPLOAD_FOLDER'])
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+if __name__ == "__main__":
+    app.run()
